016a56023f76f424a164a734677d4504c5f5d419
Firly Arie Azland <arieazland@gmail.com>
2021-06-18 07:44:54 +0700

    setup lanjutan

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(Express.urlencoded({ extended: false }));
 app.use(Express.json());
 
 /** define router */
-// app.use('/', require('./routes/pages'));
+app.use('/', require('./routes/pages'));
 // app.use('/auth', require('./routes/auth'));
 // app.use('/konsul', require('./routes/konsul'));
 // app.use('/partisipant', require('./routes/partisipant'));
---
+++ routes/pages.js
@@ -0,0 +1,17 @@
+const Express = require("express");
+const Router = Express.Router();
+const Dotenv = require("dotenv");
+// Set Moment Format engine
+const Moment = require("moment");
+require("moment/locale/id");  // without this line it didn't work
+Moment.locale('id');
+
+Dotenv.config({ path: './.env' });
+const Connection = require ("../DBconnection");
+
+/** Route for Register */
+Router.get('/', (req, res) => {
+    res.send("Hello, welcome to API-SIASEM Page")
+})
+
+module.exports = Router;
