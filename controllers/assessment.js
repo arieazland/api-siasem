f7ecb0b303f23f1d47bd756cc8953221dd85e6d5
Firly Arie Azland <arieazland@gmail.com>
2021-08-09 21:18:01 +0700

    Update assessment.js

--- controllers/assessment.js
+++ controllers/assessment.js
@@ -12,51 +12,85 @@ Moment.locale('id');
 
 /** Insert Acara Process */
 exports.registrasiJawaban = async (req, res) => {
-    try{
-        const { idacara, iduser, idsoal, radio } = req.body;
-        var tanggal = Moment().format("YYYY-MM-DD");
-        var waktu = Moment().format("HH:mm:ss");
-        
-        if(idacara && iduser && idsoal && radio){
-            // var sql = "INSERT INTO t_answer (id, iduser, idsoal, jawab, idacara, date_created, time_created) VALUES ?";
-            // var value = [];
+    const { idacara, iduser, idsoal, radio } = req.body;
+    var tanggal = Moment().format("YYYY-MM-DD");
+    var waktu = Moment().format("HH:mm:ss");
+    
+    if(idacara && iduser && idsoal && radio){
+        try{
             for( var i = 0; i < idsoal.length; i++){
-                // value.push([null, iduser, idsoal[i], radio[i], idacara, tanggal, waktu]);
-                var inputjawaban = Connection.query("INSERT INTO t_answer SET ?", [{id: null, iduser: iduser, idsoal: idsoal[i], jawab: radio[i], idacara: idacara, date_created: tanggal, time_created: waktu}], async(error, results)=>{})
+                var simpan_jawaban = await new Promise((resolve, reject) => {
+                    Connection.query("INSERT INTO t_answer SET ?", [{id: null, iduser: iduser, idsoal: idsoal[i], jawab: radio[i], idacara: idacara, date_created: tanggal, time_created: waktu}], (error, results)=>{
+                        if(error) { 
+                            reject(error);
+                        } else {
+                            resolve("true");
+                        }
+                    })
+                })
             }
-            if(inputjawaban){
-                /** input jawaban berhasil */
+
+            if (simpan_jawaban === "true") {
+                /** jika jawaban berhasil disimpan */
                 res.status(201).json({
                     message: "Jawaban berhasil disimpan, silahkan melanjutkan",
-                        idacara
+                    idacara
                 });
             } else {
-                res.status(500).json({
-                    message: "Jawaban gagal disimpan"
-                });
+                /** jika jawaban gagal disimpan */
+                throw new Error('Jawaban gagal disimpan');
             }
-            // Connection.query(sql, [value], async (error, results) => {
-            //     if(error){
-            //         res.status(500).json({
-            //             message: error
-            //         });
-            //     } else {
-            //         /** Field tidak boleh kosong */
-            //         res.status(201).json({
-            //             message: "Jawaban berhasil disimpan, silahkan melanjutkan",
-            //              idacara
-            //         });
-            //     }
-            // })
-        } else {
-            /** Field tidak boleh kosong */
-            res.status(403).json({
-                message: "Field tidak boleh kosong",
-            });
+        } catch(e) {
+            /** send error */
+            res.status(400).json({ message: e.message });
         }
-    } catch(error) {
-        res.status(500).json({
-            message: error
+    } else {
+        /** Field tidak boleh kosong */
+        res.status(403).json({
+            message: "Field tidak boleh kosong",
+        });
+    }
+}
+
+exports.registrasiJawabanV1 = async (req, res) => {
+    
+    const { idacara, iduser, idsoal, radio } = req.body;
+    var tanggal = Moment().format("YYYY-MM-DD");
+    var waktu = Moment().format("HH:mm:ss");
+    
+    if(idacara && iduser && idsoal && radio){
+        try{
+            var sql = "INSERT INTO t_answer (id, iduser, idsoal, jawab, idacara, date_created, time_created) VALUES ?";
+            var value = [];
+            for( var i = 0; i < idsoal.length; i++){
+                value.push([null, iduser, idsoal[i], radio[i], idacara, tanggal, waktu]);
+            }
+        
+            const simpan_jawaban = await new Promise((resolve, reject) => {
+                Connection.query(sql, [value], (error, results) => {
+                    if(error) { 
+                        reject(error);
+                    } else {
+                        resolve(results);
+                    }
+                });
+            })
+
+            if (simpan_jawaban) {
+                res.status(201).json({
+                    message: "Jawaban berhasil disimpan, silahkan melanjutkan",
+                    idacara
+                });
+            } else {
+                throw new Error('Jawaban gagal disimpan');
+            }
+        } catch(e) {
+            res.status(400).json({ message: e.message });
+        }
+    } else {
+        /** Field tidak boleh kosong */
+        res.status(403).json({
+            message: "Field tidak boleh kosong",
         });
     }
 }
